6b1fc7908e7e24f336889bab9d5eecf9b148eedd
Tomasz Mikus <tomasz@spiritai.com>
2022-12-03 03:33:48 +0000

    Setup a basic version of the clean-up script.

--- content.js
+++ content.js
@@ -1,1 +1,29 @@
-// Here goes the content of the script
+function cleanUpElementWithFullName(node) {
+
+}
+
+function cleanUpElementsWithFullName() {
+  forEachElementWithFullName(cleanUpElementWithFullName)
+}
+
+function forEachElementWithFullName(callback) {
+  const xpath = "//[text()='Jamie Cai']";
+  const matchingElement = document.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null,
+  );
+  for (let itemIndex = 0; itemIndex < matchingElement.snapshotLength; itemIndex++) {
+    const item = matchingElement.snapshotItem(itemIndex);
+    callback(item);
+  }
+}
+
+function removeUndesiredTerms() {
+  cleanUpElementsWithFullName();
+}
+
+removeUndesiredTerms();
+setInterval(removeUndesiredTerms, 100);
